8edde64c3789bde102946efa18ea081bb617c310
Gabriel Costa Toledo <gabrielcostatoledo@outlook.com>
2024-01-05 17:05:51 -0300

    feat: add SessionProvider from next-auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,15 @@
+import type { AppProps } from "next/app";
+
+import { SessionProvider } from "next-auth/react";
+
 import Header from "@/components/header";
 import "@/styles/globals.css";
-import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <SessionProvider session={pageProps.session}>
       <Header />
       <Component {...pageProps} />
-    </>
+    </SessionProvider>
   );
 }
