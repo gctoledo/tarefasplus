b2a5739e17d44d75d615a0123f01419e24d8b539
Gabriel Costa Toledo <gabrielcostatoledo@outlook.com>
2024-01-04 23:21:59 -0300

    feat: add home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,40 @@
 import Head from "next/head";
 import styles from "@/styles/home.module.css";
 
+import Image from "next/image";
+import HeroImg from "../../public/assets/hero.png";
+
 export default function Home() {
   return (
-    <>
+    <div className={styles.container}>
       <Head>
         <title>Tarefas+</title>
       </Head>
 
-      <h1>Meu projeto</h1>
-    </>
+      <main className={styles.main}>
+        <div className={styles.logoContent}>
+          <Image
+            className={styles.hero}
+            alt="Logo Tarefas+"
+            src={HeroImg}
+            priority
+          />
+
+          <h1 className={styles.title}>
+            Sistema feito para você organizar <br />
+            seus estudos e tarefas
+          </h1>
+
+          <div className={styles.infoContent}>
+            <section className={styles.box}>
+              <span>+12 posts</span>
+            </section>
+            <section className={styles.box}>
+              <span>+90 comentários</span>
+            </section>
+          </div>
+        </div>
+      </main>
+    </div>
   );
 }
