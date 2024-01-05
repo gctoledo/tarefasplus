2812737c04e56e22f781eec8dc5a1aa5fd6bd887
Gabriel Costa Toledo <gabrielcostatoledo@outlook.com>
2024-01-05 16:25:16 -0300

    feat: implement Google authentication for login

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,8 +1,12 @@
+import { useSession, signIn, signOut } from "next-auth/react";
+
 import Link from "next/link";
 
 import styles from "./styles.module.css";
 
 const Header = () => {
+  const { data: session, status } = useSession();
+
   return (
     <header className={styles.header}>
       <section className={styles.content}>
@@ -13,12 +17,27 @@ const Header = () => {
             </h1>
           </Link>
 
-          <Link className={styles.link} href={"/dashboard"}>
-            Meu painel
-          </Link>
+          {session && (
+            <Link className={styles.link} href={"/dashboard"}>
+              Meu painel
+            </Link>
+          )}
         </nav>
 
-        <button className={styles.loginButton}>Acessar</button>
+        {status === "loading" ? (
+          <></>
+        ) : session ? (
+          <button className={styles.loginButton} onClick={() => signOut()}>
+            Olá, {session.user?.name}
+          </button>
+        ) : (
+          <button
+            className={styles.loginButton}
+            onClick={() => signIn("google")}
+          >
+            Acessar
+          </button>
+        )}
       </section>
     </header>
   );
