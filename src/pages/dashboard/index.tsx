4a3fe50f3805224709c50c7ad6d0d792770e17c5
Gabriel Costa Toledo <gabrielcostatoledo@outlook.com>
2024-01-05 19:43:46 -0300

    feat: add static task list

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -8,6 +8,9 @@ import { getSession } from "next-auth/react";
 
 import TextArea from "@/components/textarea";
 
+import { FiShare2 } from "react-icons/fi";
+import { FaTrash } from "react-icons/fa";
+
 const Dashboard = () => {
   return (
     <div className={styles.container}>
@@ -31,6 +34,26 @@ const Dashboard = () => {
               </button>
             </form>
           </div>
+        </section>
+
+        <section className={styles.taskContainer}>
+          <h1>Minhas tarefas</h1>
+
+          <article className={styles.task}>
+            <div className={styles.tagContainer}>
+              <label className={styles.tag}>PÚBLICA</label>
+              <button className={styles.shareButton}>
+                <FiShare2 color="#3183ff" size={22} />
+              </button>
+            </div>
+
+            <div className={styles.taskContent}>
+              <p>Minha primeira tarefa!</p>
+              <button className={styles.trashButton}>
+                <FaTrash color="#ea3140" size={24} />
+              </button>
+            </div>
+          </article>
         </section>
       </main>
     </div>
