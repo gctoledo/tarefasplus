e45221a6bca97f2b66f4488cdda44b62bb32f3a3
Gabriel Costa Toledo <gabrielcostatoledo@outlook.com>
2024-01-06 01:41:02 -0300

    feat: add comment feature

--- src/pages/task/[id].tsx
+++ src/pages/task/[id].tsx
@@ -1,10 +1,21 @@
+import { ChangeEvent, FormEvent, useState } from "react";
+
+import { useSession } from "next-auth/react";
+
 import { GetServerSideProps } from "next";
 
 import Head from "next/head";
 
 import styles from "./styles.module.css";
 
-import { doc, collection, query, where, getDoc } from "firebase/firestore";
+import {
+  doc,
+  collection,
+  query,
+  where,
+  getDoc,
+  addDoc,
+} from "firebase/firestore";
 import { db } from "@/services/firebaseConnection";
 
 import TextArea from "@/components/textarea";
@@ -20,6 +31,32 @@ interface TaskProps {
 }
 
 const Task = ({ task }: TaskProps) => {
+  const { data: session } = useSession();
+
+  const [input, setInput] = useState("");
+
+  const handleComment = async (e: FormEvent) => {
+    e.preventDefault();
+
+    if (input === "") return;
+
+    if (!session?.user?.email || !session?.user?.name) return;
+
+    try {
+      const docRef = await addDoc(collection(db, "comments"), {
+        comment: input,
+        created: new Date(),
+        user: session?.user?.email,
+        name: session?.user?.name,
+        taskId: task.id,
+      });
+
+      setInput("");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div className={styles.container}>
       <Head>
@@ -36,9 +73,17 @@ const Task = ({ task }: TaskProps) => {
 
       <section className={styles.commentsContainer}>
         <h2>Deixar comentário</h2>
-        <form>
-          <TextArea placeholder="Digite o seu comentário" />
-          <button className={styles.button}>Enviar comentário</button>
+        <form onSubmit={handleComment}>
+          <TextArea
+            placeholder="Digite o seu comentário"
+            value={input}
+            onChange={(e: ChangeEvent<HTMLTextAreaElement>) =>
+              setInput(e.target.value)
+            }
+          />
+          <button className={styles.button} disabled={!session?.user}>
+            Enviar comentário
+          </button>
         </form>
       </section>
     </div>
@@ -73,6 +118,7 @@ export const getServerSideProps: GetServerSideProps = async ({ params }) => {
   }
 
   const miliseconds = snapshot.data()?.created?.seconds * 1000;
+
   const task = {
     task: snapshot.data()?.task,
     public: snapshot.data()?.public,
